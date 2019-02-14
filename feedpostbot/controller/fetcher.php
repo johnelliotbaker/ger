acb3401c206e0ae6db939ec5d0cf64ceba5a4107
johnelliotbaker <johnelliotbaker@gmail.com>
2019-02-14 01:48:25 -0800

    Reject non moderators

--- feedpostbot/controller/fetcher.php
+++ feedpostbot/controller/fetcher.php
@@ -1,18 +1,39 @@
 <?php
 namespace ger\feedpostbot\controller;
 use \Symfony\Component\HttpFoundation\Response;
+use \phpbb\auth\auth;
+use \ger\feedpostbot\classes\driver;
 
 class fetcher
 {
     protected $feedpostbot;
+    protected $auth;
 
-    public function __construct(\ger\feedpostbot\classes\driver $feedpostbot)
+    public function __construct(
+        driver $feedpostbot,
+        auth $auth
+    )
     {
         $this->feedpostbot = $feedpostbot;
+        $this->auth = $auth;
+    }
+
+    public function is_mod()
+    {
+        return $this->auth->acl_gets('a_', 'm_');
+    }
+
+    public function reject_non_moderator()
+    {
+        if (!$this->is_mod())
+        {
+            trigger_error('Only moderators can access this page.');
+        }
     }
 
     public function handle()
     {
+        $this->reject_non_moderator();
         $mode = '';
         switch ($mode)
         {
@@ -20,7 +41,7 @@ class fetcher
             $this->feedpostbot->fetch_all();
             break;
         }
-        trigger_error('Fetching Feed...');
+        trigger_error('Done fetching.');
     }
 
 }
